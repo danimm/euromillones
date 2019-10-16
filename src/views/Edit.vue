39ef2fb1a789f4157d3c3da5aa78280cda11f42a
Daniel Muñoz <info@gmail.com>
2019-10-16 22:02:25 +0200

    vista edit sin componentes

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Este es el del cupon: {{ this.$route.params.id }}</h1>
+    <h1>Este es el Id del cupon: {{ this.$route.params.id }}</h1>
     <div class="row pt-4">
       <div class="col-3 offset-3">
         <!-- Fecha -->
@@ -8,41 +8,33 @@
           <div class="input-group-prepend">
             <span class="input-group-text" id="fecha">Fecha: {{ data.fecha }}</span>
           </div>
-          <input type="text" class="form-control" placeholder="Nuevo valor" aria-label="Username" />
+          <input type="text" class="form-control" placeholder="Nuevo valor" :value="data.fecha" />
         </div>
         <!-- Fin de Fecha -->
         <!-- Estrellas -->
         <div class="input-group mb-3" v-for="star in data.stars" :key="star.id">
           <div class="input-group-prepend">
-            <span class="input-group-text span-estrellas">Estrellas: {{ star }}</span>
+            <span class="input-group-text span-estrellas">Estrellas:</span>
           </div>
-          <input type="text" class="form-control" placeholder="Nuevo valor" />
+          <input type="text" class="form-control" placeholder="Nuevo valor" :value="star" />
         </div>
         <!-- Fin de Estrellas -->
         <!-- Ganancias -->
         <div class="input-group mb-3">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="ganancias">Ganancias: {{ data.ganancias}}</span>
+            <span class="input-group-text" id="ganancias">Ganancias: {{ data.ganancias }}</span>
           </div>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Nuevo valor"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
+          <input type="text" class="form-control" placeholder="Nuevo valor" :value="data.ganancias" />
         </div>
         <!-- Fin de Ganancias -->
       </div>
       <div class="col-3">
         <!-- Números -->
-        <div class="input-group mb-3" v-for="number in data.numbers" :key="number.id">
+        <div class="input-group mb-3" v-for="(number, index) in data.numbers" :key="index">
           <div class="input-group-prepend">
-            <span
-              class="input-group-text span-numeros"
-            >Número {{ data.numbers[number] }}: {{ number }}</span>
+            <span class="input-group-text span-numeros">Número {{ index+1 }}:</span>
           </div>
-          <input type="text" class="form-control" placeholder="Nuevo valor" />
+          <input type="text" class="form-control" placeholder="Nuevo valor" :value="number" />
         </div>
         <!-- Fin de Números -->
       </div>
@@ -57,13 +49,7 @@
 
 <script>
 import db from "../firebase.js";
-import inputComponent from "../components/Edit/inputComponent.vue";
-import inputVforComponent from "../components/Edit/inputVforComponent.vue";
 export default {
-  components: {
-    inputComponent,
-    inputVforComponent
-  },
   data() {
     return {
       data: {}
